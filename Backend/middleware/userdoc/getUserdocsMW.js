f9fa59623116bd0abbbec5b8c49c9b9672dbbf86
Nagy Ádám <nagy.addam@gmail.com>
2022-10-24 02:01:11 +0200

    get Userdocs contains owner and type

--- Backend/middleware/userdoc/getUserdocsMW.js
+++ Backend/middleware/userdoc/getUserdocsMW.js
@@ -3,23 +3,47 @@ const requireOption = require('../generic/requireOption');
 module.exports = function (objectrepository) {
 
     const UserdocModel = requireOption(objectrepository, 'UserdocModel');
+    const DoctypeModel = requireOption(objectrepository, 'DoctypeModel');
+    const UserModel = requireOption(objectrepository, 'UserModel');
 
     return function (req, res, next) {
         if (res.locals.authenticatedUser.role === 'Administrator' || res.locals.authenticatedUser.role === 'Superadmin') {
-            UserdocModel.find({}, (err, userdocs) => {
+            UserdocModel.find({}, async (err, userdocs) => {
                 if (err) {
                     return next(err);
                 }
-                res.locals.userdocs = userdocs;
+                res.locals.userdocs = [];
+                for ([index, element] of userdocs.entries()) {
+                    const doctype = await DoctypeModel.findOne({ _id: element._doctype });
+                    const owner = await UserModel.findOne({ _id: element._owner });
+                    res.locals.userdocs[index] = {};
+                    res.locals.userdocs[index]._id = element._id;
+                    res.locals.userdocs[index].details = element.details;
+                    res.locals.userdocs[index].expires_at = element.expires_at;
+                    res.locals.userdocs[index].name = element.name;
+                    res.locals.userdocs[index].owner = {_id: owner._id, name: owner.name};
+                    res.locals.userdocs[index].doctype = doctype;
+                }
                 return next();
             });
         }
-        else{
-            UserdocModel.find({_owner: res.locals.authenticatedUser.userId}, (err, userdocs) => {
+        else {
+            UserdocModel.find({ _owner: res.locals.authenticatedUser.userId }, async (err, userdocs) => {
                 if (err) {
                     return next(err);
                 }
-                res.locals.userdocs = userdocs;
+                res.locals.userdocs = [];
+                for ([index, element] of userdocs.entries()) {
+                    const doctype = await DoctypeModel.findOne({ _id: element._doctype });
+                    const owner = await UserModel.findOne({ _id: element._owner });
+                    res.locals.userdocs[index] = {};
+                    res.locals.userdocs[index]._id = element._id;
+                    res.locals.userdocs[index].details = element.details;
+                    res.locals.userdocs[index].expires_at = element.expires_at;
+                    res.locals.userdocs[index].name = element.name;
+                    res.locals.userdocs[index].owner = {_id: owner._id, name: owner.name};
+                    res.locals.userdocs[index].doctype = doctype;
+                }
                 return next();
             });
         }
